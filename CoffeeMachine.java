02539f5a9685be442e46c99440d71d3392c04f64
DenisOlaleye <denis.olaleye@gmail.com>
2022-09-19 16:48:56 +0300

    ||

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -4,11 +4,13 @@ public class CoffeeMachine
 	public static void main(String[] args) {
 		int cofeeAmount = 2330;
 		int milkAmount = 3210;
+		int skimmedMilk = 1600;
+		
 		
 		int cappucinoMilkRequird = 60;
 		int cappucinoCoffeRequired = 15;
 		
-		if (cofeeAmount >= cappucinoCoffeRequired && milkAmount >= cappucinoMilkRequird)
+		if (skimmedMilk >= cappucinoMilkRequird || milkAmount >= cappucinoMilkRequird)
 		{
 			System.out.println("Готуємо капучино...");
 		}
